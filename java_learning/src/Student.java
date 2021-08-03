77bf07b74098909d9246770b246c1cb455c4a413
hm <liumengff@gmail.com>
2021-08-03 21:42:13 +0800

    add

---
+++ java_learning/src/Student.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+//定义一个标准类，包括成员函数、成员变量（私有）、构造方法、成员方法
+public class Student {
+    private String name;
+    private int age;
+    public Student(){}
+    public Student(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+    public void show(){
+        System.out.println("name:"+name);
+        System.out.println("age:"+age);
+
+    }
+
+
+
+}
---
+++ java_learning/src/StudentDemo.java
@@ -0,0 +1,7 @@
+public class StudentDemo {
+    public static void main(String[] args) {
+        Student std = new Student("Mengfan Liu",24);
+        std.show();
+
+    }
+}
