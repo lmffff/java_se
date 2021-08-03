0e59b0983a8deb032e91cb90c24696f264dcad96
hm <liumengff@gmail.com>
2021-08-03 21:42:13 +0800

    不同的字符串构造方法

---
+++ java_learning/src/RandomNumber.java
@@ -0,0 +1,11 @@
+
+// use random class to output a number (0,10)
+import java.util.Random;
+
+
+public class RandomNumber {
+    public static void main(String[] args) {
+        Random ra = new Random();
+        System.out.println(ra.nextInt(10));
+    }
+}
---
+++ java_learning/src/StringDemo.java
@@ -0,0 +1,19 @@
+//创建字符串对象，并输出
+public class StringDemo {
+    public static void main(String[] args) {
+        String s1 = new String();   //空字符串
+        System.out.println("s1:"+s1);
+
+        char[] chs = {'a','b','c'};  //字符数组
+        String s2 = new String(chs);
+        System.out.println("s2:"+s2);
+
+        char[] bys = {120,121,122};  //字节数组 ASCII码
+        String s3 = new String(bys);
+        System.out.println("s3:"+s3);
+
+        String s4 = "1234";      //常量
+        System.out.println(s4);
+
+    }
+}
