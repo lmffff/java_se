0159fa8c1c131b23e88a53a494b99751fccb9b36
hm <liumengff@gmail.com>
2021-08-03 20:09:21 +0800

    add

---
+++ java_learning/src/HelloWorld.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int input = sc.nextInt();
+        String input = "Hello world!";
+        System.out.println(input);
+    }
+}
