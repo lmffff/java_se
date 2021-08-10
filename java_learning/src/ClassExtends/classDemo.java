48111fc51bd68cc3ce7fb24e849e63c3aeedaa4e
hm <liumengff@gmail.com>
2021-08-10 15:26:33 +0800

    ClassBasic

--- java_learning/src/ClassExtends/classDemo.java
+++ java_learning/src/ClassExtends/classDemo.java
@@ -4,7 +4,7 @@ public class classDemo {
     public static void main(String[] args) {
         Dog dog1 = new Dog();
         Dog dog2 = new Dog("胖虎");
-        dog1.className = "dog";
+        Dog.className = "dog";
         System.out.println(dog1.className);
         System.out.println(dog2.className);
 
