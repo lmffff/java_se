4c761dcc53682eac4de11e80b287dda3d913c2ed
hm <liumengff@gmail.com>
2021-08-11 10:58:43 +0800

    内部类、外部类

--- java_learning/src/ClassExtends/Outer.java
+++ java_learning/src/ClassExtends/Outer.java
@@ -1,4 +1,18 @@
 package ClassExtends;
 
 public class Outer {
+    private int num = 10;
+    public class Inner{
+        public void show(){    //内部类可以直接访问外部类
+            System.out.println("num:"+num);
+        }
+    }
+
+    public void method(){
+//        show();    外部类不可以直接访问内部类
+        Inner i = new Inner();
+        i.show();
+
+    }
+
 }
