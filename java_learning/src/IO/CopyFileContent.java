a5a2cea3dd0b5f6a687588b965c1d3ab08f29eb2
hm <liumengff@gmail.com>
2021-08-19 15:32:43 +0800

    update

--- java_learning/src/IO/CopyFileContent.java
+++ java_learning/src/IO/CopyFileContent.java
@@ -2,37 +2,62 @@ package IO;
 
 import java.io.*;
 
-// 用字节流复制、用fileWriter、fileReader定义（只是简化了定义过程，写入方式不变）+字符数组写入
+// 1.用字节流复制;
+// 2.用fileWriter、fileReader定义（只是简化了定义过程，写入方式不变）+字符数组写入;
+// 3.buffered
 public class CopyFileContent {
     public static void main(String[] args) throws IOException {
-        File co = new File("javaIO\\copyFrom.txt");
-        File pa = new File("javaIO\\copyTo.txt");
-        co.createNewFile();
-        pa.createNewFile();
 
-        FileOutputStream fos = new FileOutputStream(co);
-        fos.write("jyzdqdsuhdagfiuegi".getBytes());
-        fos.close();
+        //1.用字节流复制;
+//        File co = new File("javaIO\\copyFrom.txt");
+//        File pa = new File("javaIO\\copyTo.txt");
+//        co.createNewFile();
+//        pa.createNewFile();
+//
+//        FileOutputStream fos = new FileOutputStream(co);
+//        fos.write("jyzdqdsuhdagfiuegi".getBytes());
+//        fos.close();
+//
+//        FileInputStream fis = new FileInputStream(co);
+//        FileOutputStream fos1 = new FileOutputStream(pa);
+//        int temp;
+//        while((temp = fis.read())!=-1){
+//            fos1.write(temp);
+//        }
+//        fis.close();
+//        fos1.close();
 
-        FileInputStream fis = new FileInputStream(co);
-        FileOutputStream fos1 = new FileOutputStream(pa);
-        int temp;
-        while((temp = fis.read())!=-1){
-            fos1.write(temp);
-        }
-        fis.close();
-        fos1.close();
 
-        FileReader fr = new FileReader("javaIO\\copyFrom.txt");
-        FileWriter fw = new FileWriter("javaIO\\copyTo.txt");
-        char[] chs = new char[1024];
-        int len;
-        while((len = fr.read(chs))!=-1){
-            fw.write(new String(chs,0,len));
-        }
-        fr.close();
-        fw.close();
 
+
+        //2.用fileWriter、fileReader定义（只是简化了定义过程，写入方式不变）+字符数组写入;
+//        FileReader fr = new FileReader("javaIO\\copyFrom.txt");
+//        FileWriter fw = new FileWriter("javaIO\\copyTo.txt");
+//        char[] chs = new char[1024];
+//        int len;
+//        while((len = fr.read(chs))!=-1){
+//            fw.write(new String(chs,0,len));
+//        }
+//        fr.close();
+//        fw.close();
+
+
+
+        //3.buffered
+        BufferedWriter bw = new BufferedWriter(new FileWriter("javaIO\\copyFrom.txt"));
+        bw.write("BufferedWriter");
+        bw.write("hello");
+
+        bw.close();
+//        BufferedWriter bw1 = new BufferedWriter(new FileWriter("javaIO\\copyTo.txt"));
+//        BufferedReader br1 = new BufferedReader(new FileReader("javaIO\\copyFrom.txt"));
+//        char[] chs = new char[3];
+//        int len;
+//        while((len = br1.read(chs))!=-1){
+//            bw1.write(chs,0,len);
+//        }
+//        bw1.close();
+//        br1.close();
 
     }
 
